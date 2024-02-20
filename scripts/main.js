a6e885ab2a68333797d397b9f1ff2d534472bfe6
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-02-20 14:00:31 +0000

    Add playRound function
    Add playRound function to accept player choice and determine the outcome of the game

--- scripts/main.js
+++ scripts/main.js
@@ -11,5 +11,60 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+// Play a round of rps and determine winner
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    const formattedPlayer = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    const formattedComputer = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+    let outcome;
+    switch (playerSelection) {
+        case "rock":
+            switch (computerSelection) {
+                case "rock":
+                    outcome = "draw";
+                    break;
+                case "paper":
+                    outcome = "lose";
+                    break;
+                default:
+                    outcome = "win";
+            }
+            break;
+        case "paper":
+            switch (computerSelection) {
+                case "rock":
+                    outcome = "win";
+                    break;
+                case "paper":
+                    outcome = "draw";
+                    break;
+                default:
+                    outcome = "lose";
+            }
+            break;
+        default:
+            switch (computerSelection) {
+                case "rock":
+                    outcome = "lose";
+                    break;
+                case "paper":
+                    outcome = "win";
+                    break;
+                default:
+                    outcome = "draw";
+            }
+    }
+    switch (outcome) {
+        case "win":
+            return ("You win! " + formattedPlayer + " beats " + formattedComputer);
+        case "lose":
+            return ("You lose! " + formattedComputer + " beats " + formattedPlayer);
+        default:
+            return("You draw! " + formattedPlayer + " draws with " + formattedComputer);
+    }
+}
 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
