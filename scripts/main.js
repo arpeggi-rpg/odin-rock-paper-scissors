adeb5707da6d92005ce4b8b17591ba997cb5dca8
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-02-21 14:41:03 +0000

    Add playGame function
    This function acts as the game loop

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-// getComputerChoice - randomly return 'rock', 'paper' or 'scissors'
 function getComputerChoice() {
     result = Math.floor(Math.random() * 3);
     switch (result) {
@@ -11,60 +10,71 @@ function getComputerChoice() {
     }
 }
 
-// Play a round of rps and determine winner
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    const formattedPlayer = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    const formattedComputer = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-    let outcome;
     switch (playerSelection) {
         case "rock":
             switch (computerSelection) {
                 case "rock":
-                    outcome = "draw";
-                    break;
+                    return "draw";
                 case "paper":
-                    outcome = "lose";
-                    break;
+                    return "lose";
                 default:
-                    outcome = "win";
+                    return "win";
             }
-            break;
         case "paper":
             switch (computerSelection) {
                 case "rock":
-                    outcome = "win";
-                    break;
+                    return "win";
                 case "paper":
-                    outcome = "draw";
-                    break;
+                    return "draw";
                 default:
-                    outcome = "lose";
+                    return "lose";
             }
-            break;
         default:
             switch (computerSelection) {
                 case "rock":
-                    outcome = "lose";
-                    break;
+                    return "lose";
                 case "paper":
-                    outcome = "win";
-                    break;
+                    return "win";
                 default:
-                    outcome = "draw";
+                    return "draw";
             }
-    }
-    switch (outcome) {
-        case "win":
-            return ("You win! " + formattedPlayer + " beats " + formattedComputer);
-        case "lose":
-            return ("You lose! " + formattedComputer + " beats " + formattedPlayer);
-        default:
-            return("You draw! " + formattedPlayer + " draws with " + formattedComputer);
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i=0; i<5; i++) {
+        let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+        let computerSelection = getComputerChoice();
+        const formattedPlayer = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        const formattedComputer = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        let outcome = playRound(playerSelection, computerSelection);
+        switch (outcome) {
+            case "win":
+                console.log("You win! " + formattedPlayer + " beats " + formattedComputer);
+                playerScore++;
+                break;
+            case "lose":
+                console.log("You lose! " + formattedComputer + " beats " + formattedPlayer);
+                computerScore++;
+                break;
+            default:
+                console.log("You draw! " + formattedPlayer + " draws with " + formattedComputer);
+        }
+    }
+    console.log("Player - " + playerScore + " | Computer - " + computerScore);
+    if (playerScore > computerScore) {
+        console.log("Player Wins!");
+    }
+    else if (playerScore == computerScore) {
+        console.log("It's a draw!");
+    }
+    else {
+        console.log("Computer Wins!")
+    }
+    return;
+}
+
+playGame();
